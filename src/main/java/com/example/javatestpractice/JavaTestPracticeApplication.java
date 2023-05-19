a9d062aef89412d2ff94b9208b583fb44e7f194d
mauv2sky <mauv2sky@gmail.com>
2023-05-19 17:20:08 +0900

    Initial commit

---
+++ src/main/java/com/example/javatestpractice/JavaTestPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.javatestpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTestPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javatestpractice/JavaTestPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javatestpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
